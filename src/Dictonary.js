fc195c7bf9d7336f8c39f7496b10be2ecb807bae
Franziska Wolf <franzy_wolf@web.de>
2022-08-06 11:16:04 +0200

    adding axios and api

--- src/Dictonary.js
+++ src/Dictonary.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './Dictonary.css'
 
 export default function Dictonary() {
     let [input, setInput] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function search(event) {
         event.preventDefault();
-        alert(input);
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleInputChange(event) {
