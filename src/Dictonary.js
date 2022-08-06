9d71658039e30be2c17b191eea55a231aa7e5572
Franziska Wolf <franzy_wolf@web.de>
2022-08-06 10:57:10 +0200

    adding the searchengine

--- src/Dictonary.js
+++ src/Dictonary.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dictonary.css'
 
 export default function Dictonary() {
+    let [input, setInput] = useState("");
+
+    function search(event) {
+        event.preventDefault();
+        alert(input);
+    }
+
+    function handleInputChange(event) {
+        setInput(event.target.value);
+    }
+
     return (
         <div className="Dictonary">
-            <form>
-                <input type="search" />
+            <form onSubmit={search}>
+                <input type="search" onChange={handleInputChange} />
             </form>
         </div>
     )
